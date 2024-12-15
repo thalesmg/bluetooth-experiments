a207f70ef4cb40bbe4be64c10a0042b2f94f6269
Thales MG <>
2024-12-15 14:49:47 -0300

    mais derrota

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,15 @@
 use std::ffi::CStr;
 
+use libc::uint8_t;
 use nix::errno::Errno;
 
 pub mod bindings;
 
 use bindings::{
-    hci_get_route, hci_inquiry, hci_open_dev, hci_read_class_of_dev, hci_read_inquiry_mode,
-    hci_read_inquiry_scan_type, hci_read_local_name, hci_read_remote_name, hci_request,
-    hci_send_req, hci_write_class_of_dev, hci_write_inquiry_mode, hci_write_inquiry_scan_type,
-    hci_write_local_name, inquiry_info, IREQ_CACHE_FLUSH, OCF_READ_PAGE_SCAN_TYPE,
-    OCF_WRITE_PAGE_SCAN_TYPE, OCF_WRITE_SCAN_ENABLE, OGF_HOST_CTL,
+    hci_get_route, hci_inquiry, hci_open_dev, hci_read_class_of_dev, hci_read_inquiry_mode, hci_read_inquiry_scan_type, hci_read_local_name, hci_read_remote_name, hci_request, hci_send_req, hci_write_class_of_dev, hci_write_inquiry_mode, hci_write_inquiry_scan_type, hci_write_local_name, inquiry_info, IREQ_CACHE_FLUSH, OCF_READ_PAGE_SCAN_TYPE, OCF_RESET, OCF_SET_EVENT_FLT, OCF_WRITE_PAGE_SCAN_TYPE, OCF_WRITE_SCAN_ENABLE, OGF_HOST_CTL
 };
 
-const MAX_NAME_LENGTH: i32 = 250;
+const MAX_NAME_LENGTH: i32 = 248;
 const MAX_INQUIRIES: i32 = 255;
 
 #[repr(C, packed)]
@@ -43,6 +40,25 @@ struct WriteScanEnableCp {
 #[repr(C, packed)]
 #[derive(Debug, Copy, Clone)]
 struct WriteScanEnableRp {
+    status: u8,
+}
+
+#[repr(C, packed)]
+#[derive(Debug, Copy, Clone)]
+struct SetEventFilterCp {
+    type_: u8,
+    condition: u8,
+}
+
+#[repr(C, packed)]
+#[derive(Debug, Copy, Clone)]
+struct SetEventFilterRp {
+    status: u8,
+}
+
+#[repr(C, packed)]
+#[derive(Debug, Copy, Clone)]
+struct ResetRp {
     status: u8,
 }
 
@@ -84,7 +100,8 @@ unsafe fn hci_write_page_scan_type(
         ogf: OGF_HOST_CTL as _,
         ocf: OCF_WRITE_PAGE_SCAN_TYPE as _,
         cparam: p_cp,
-        clen: size_of::<WritePageScanTypeCp> as _,
+        // clen: size_of::<WritePageScanTypeCp> as _,
+        clen: 1,
         rparam: p_rp,
         rlen: size_of::<WritePageScanTypeRp>() as _,
         ..req
@@ -106,9 +123,54 @@ unsafe fn hci_write_scan_enable(bt_socket: bt_socket, type_: u8, timeout: timeou
         ogf: OGF_HOST_CTL as _,
         ocf: OCF_WRITE_SCAN_ENABLE as _,
         cparam: p_cp,
-        clen: size_of::<WriteScanEnableCp> as _,
+        // clen: size_of::<WriteScanEnableCp> as _,
+        clen: 1,
         rparam: p_rp,
         rlen: size_of::<WriteScanEnableRp>() as _,
+        ..req
+    };
+    if dbg!(hci_send_req(bt_socket, &mut req, timeout)) < 0 || rp.status == nix::Error::EIO as _ {
+        None
+    } else {
+        Some(())
+    }
+}
+
+unsafe fn hci_set_event_filter(bt_socket: bt_socket, type_: u8, condition: u8, timeout: timeout) -> Option<()> {
+    let mut cp = SetEventFilterCp { type_, condition };
+    let p_cp = &mut cp as *mut _ as *mut _;
+    let mut rp = std::mem::zeroed::<SetEventFilterRp>();
+    let p_rp = &mut rp as *mut _ as *mut _;
+    let mut req = std::mem::zeroed::<hci_request>();
+    req = hci_request {
+        ogf: OGF_HOST_CTL as _,
+        ocf: OCF_SET_EVENT_FLT as _,
+        cparam: p_cp,
+        // clen: size_of::<SetEventFilterCp> as _,
+        clen: 2,
+        rparam: p_rp,
+        rlen: size_of::<SetEventFilterRp>() as _,
+        ..req
+    };
+    if dbg!(hci_send_req(bt_socket, &mut req, timeout)) < 0 || rp.status == nix::Error::EIO as _ {
+        None
+    } else {
+        Some(())
+    }
+}
+
+unsafe fn hci_reset(bt_socket: bt_socket, timeout: timeout) -> Option<()> {
+    let mut rp = std::mem::zeroed::<ResetRp>();
+    let p_rp = &mut rp as *mut _ as *mut _;
+    let mut req = std::mem::zeroed::<hci_request>();
+    req = hci_request {
+        ogf: OGF_HOST_CTL as _,
+        ocf: OCF_RESET as _,
+        cparam: std::ptr::null_mut(),
+        // clen: size_of::<SetEventFilterCp> as _,
+        clen: 0,
+        rparam: p_rp,
+        rlen: size_of::<ResetRp>() as _,
         ..req
     };
     if dbg!(hci_send_req(bt_socket, &mut req, timeout)) < 0 || rp.status == nix::Error::EIO as _ {
@@ -156,6 +218,10 @@ fn main() {
     dbg!(unsafe { hci_read_inquiry_scan_type(bt_socket, &mut inquiry_scan_type, 1_000) });
     dbg!(inquiry_scan_type);
 
+
+
+    // dbg!(unsafe { hci_reset(bt_socket, 1_000) });
+
     dbg!(unsafe { hci_write_inquiry_mode(bt_socket, 0x01, 1_000) });
 
     let write_page_scan_type_ret = unsafe { hci_write_page_scan_type(bt_socket, 1, 2_000) };
@@ -182,6 +248,10 @@ fn main() {
     let write_scan_enable_ret = unsafe { hci_write_scan_enable(bt_socket, 0x02, 2_000) };
     dbg!(write_scan_enable_ret);
 
+    let set_event_filter_ret = unsafe { hci_set_event_filter(bt_socket, 0x01, 0x00, 1_000) };
+    dbg!(set_event_filter_ret);
+
+
     // write page scan type - interlaced scan
     // write inquiry scan type - interlaced scan
     // write class of device - 0x400204
@@ -198,16 +268,22 @@ fn main() {
             infos.push(std::mem::zeroed::<inquiry_info>());
         }
     }
-    let lap: [u8; 3] = [0x9e, 0x8b, 0x00];
+    // let lap: [u8; 3] = [0x9e, 0x8b, 0x00];
+    // let lap = vec![0x9e, 0x8b, 0x00];
+    let lap = vec![0x00, 0x8b, 0x9e];
+    dbg!(lap.as_ptr());
     let device_count = unsafe {
         hci_inquiry(
             bt_device_id,
-            6,   // scan seconds
-            255, // max inquiries
+            3,   // scan seconds
+            // 255, // max inquiries
+            // 0, // max inquiries
+            MAX_INQUIRIES,
             // std::ptr::null(), // lap (??)
             lap.as_ptr(), // lap (??)
             &mut infos.as_mut_ptr(),
             IREQ_CACHE_FLUSH as _,
+            // 0x00,
         )
     };
     dbg!(device_count);
